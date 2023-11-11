475140c6649bcdaa78a728b67820906caa3f6b58
TatianaYakushonok <yakush2@yandex.ru>
2023-11-11 09:47:14 +0700

    feat: add task 2 (Sat, 11 Nov 2023 09:47:14 +0700)

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@
   console.log('Общая стоимость: ' + totalPrice);
   console.groupEnd();
 }
+
+// Модуль 4 урок 2
+{
+  const nameGoods = prompt('Введите наименование товара');
+  const category = prompt('Введите категорию товара');
+  const amount = parseInt(prompt('Введите количество товара'));
+  const price = parseInt(prompt('Введите цену товара'));
+
+  if (!isNaN(amount) && !isNaN(price)) {
+    const totalPrice = amount * price;
+
+    console.group('Модуль 4 урок 2');
+    console.log(typeof amount);
+    console.log(typeof price);
+    console.log(`На складе ${amount} единицы товара "${nameGoods}" на сумму ${totalPrice.toLocaleString()} ₽`);
+    console.groupEnd();
+  } else {
+    console.group('Модуль 4 урок 2');
+    console.log('Вы ввели некорректные данные! Введите число');
+    console.groupEnd();
+  }
+}
