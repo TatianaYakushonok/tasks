e2ced694c9109bc545f15f720d783829e502ccec
TatianaYakushonok <yakush2@yandex.ru>
2023-11-17 09:02:20 +0700

    feat: add task game-01 (Fri, 17 Nov 2023 09:02:19 +0700)

--- js/game-01.js
+++ js/game-01.js
@@ -4,19 +4,23 @@
 
 const guessNumber = () => {
   const randomNum = Math.floor(Math.random() * 101);
-  let choiceNum;
 
-  while ((choiceNum = prompt('Введи число от 1 до 100'))) {
-    console.log(choiceNum);
-    if (isNaN(choiceNum)) {
-      alert('Введи число');
-    } else if (choiceNum > randomNum) {
-      alert('Меньше');
-    } else if (choiceNum < randomNum) {
-      alert('Больше');
-    } else if (+choiceNum === randomNum) {
-      alert('Правильно');
+  while (true) {
+    const userNum = prompt('Введи число от 1 до 100');
+
+    if (+userNum) {
+      if (+userNum > randomNum) {
+        alert('Меньше');
+      } else if (+userNum < randomNum) {
+        alert('Больше');
+      } else if (+userNum === randomNum) {
+        alert('Правильно');
+        break;
+      }
+    } else if (userNum === null) {
       break;
+    } else if (isNaN(+userNum) || +userNum === 0) {
+      alert('Введи число');
     }
   }
 };
