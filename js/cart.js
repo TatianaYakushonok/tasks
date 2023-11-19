d5a9e815894e59a58741717a0ab519609d0d93c3
TatianaYakushonok <yakush2@yandex.ru>
2023-11-19 15:29:57 +0700

    feat: add task cart (Sun, 19 Nov 2023 15:29:57 +0700)

---
+++ js/cart.js
@@ -0,0 +1,44 @@
+'use strict';
+// Модуль 4 урок 8 задача 1
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  add(name, price, count = 1) {
+    const objGoods = Object.assign(
+      {},
+      {
+        name: name,
+        price: price,
+        count: count,
+      },
+    );
+    this.items.push(objGoods);
+    this.calculateItemPrice();
+    this.increaseCount(count);
+  },
+  increaseCount(num) {
+    return (this.count += num);
+  },
+  calculateItemPrice() {
+    return (this.totalPrice = this.items.reduce((acc, item) => (acc += item.price * item.count), 0));
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(`${JSON.stringify(this.items)} \n Общая стоимость товаров: ${this.totalPrice.toLocaleString()} ₽`);
+  },
+};
+
+console.group('Модуль 4 урок 8 задача 1');
+cart.add('Телевизор LG', 45000, 10);
+cart.add('Sony PlayStation', 35000, 5);
+cart.print();
+console.groupEnd();
