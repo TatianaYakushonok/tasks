c331bbdd9894cc556a750bf5e8232cf4855dc35f
TatianaYakushonok <yakush2@yandex.ru>
2023-11-25 14:09:49 +0700

    feat: add task-01, task-this-01 (Sat, 25 Nov 2023 14:09:49 +0700)

---
+++ js/task-this-01.js
@@ -0,0 +1,30 @@
+'use strict';
+// Модуль 4 урок 11 задача 1 this
+
+const rectangle = {
+  width: 5,
+  height: 5,
+
+  set addWidth(value) {
+    this.width = value;
+  },
+  set addHeight(value) {
+    this.height = value;
+  },
+
+  get calculatePerimeter() {
+    return `${(this.width + this.height) * 2}см`;
+  },
+  get calculateArea() {
+    return `${this.width * this.height}см`;
+  },
+};
+
+console.group('Модуль 4 урок 11 задача 1 this');
+console.log(rectangle.calculatePerimeter);
+console.log(rectangle.calculateArea);
+rectangle.addWidth = 20;
+rectangle.addHeight = 10;
+console.log(rectangle.calculatePerimeter);
+console.log(rectangle.calculateArea);
+console.groupEnd();
